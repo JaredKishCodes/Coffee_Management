f1e73dd6ad17a0c7904b7fa44e9bcd19b825b705
JaredKishCodes <missionajk@gmail.com>
2025-07-30 22:36:17 +0800

    add menu page

--- coffee-frontend/coffee-frontend/src/app/app.routes.ts
+++ coffee-frontend/coffee-frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Layout } from './components/layout/layout';
 import { Login } from './components/login/login';
 import { Home } from './components/home/home';
+import { Menu } from './components/menu/menu';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: Home },
-  { path: 'login', component: Login } 
+  { path: 'login', component: Login } ,
+  {path:'menu',component:Menu},
 ];
 
--- coffee-frontend/coffee-frontend/src/app/components/home/home.ts
+++ coffee-frontend/coffee-frontend/src/app/components/home/home.ts
@@ -4,7 +4,7 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterOutlet, RouterModule,CommonModule],
+  imports: [ RouterModule,CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
