72bed81be01dd56ef0275b9a37a10a1195f7cfb8
JaredKishCodes <missionajk@gmail.com>
2025-08-01 14:51:32 +0800

    added cart entities

---
+++ coffee-backend/CoffeeManagementSystem/CoffeeManagementSystem.Domain/Entities/Cart.cs
@@ -0,0 +1,21 @@
+﻿
+
+using CoffeeManagementSystem.Domain.Enums;
+
+namespace CoffeeManagementSystem.Domain.Entities
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+
+        public required string CustomerName { get; set; }
+
+        public decimal TotalPrice { get; set; }
+
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+
+        public OrderStatus OrderStatus { get; set; } = OrderStatus.Pending;
+
+        public List<CartItem> CartItems { get; set; } = new();
+    }
+}
---
+++ coffee-backend/CoffeeManagementSystem/CoffeeManagementSystem.Domain/Entities/CartItem.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace CoffeeManagementSystem.Domain.Entities
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public int CartId { get; set; }
+        public Cart Cart { get; set; } = null!; 
+
+        public int CoffeeItemId { get; set; }
+        public CoffeeItem CoffeeItem { get; set; } = null!; 
+
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal Total { get; set; }
+    }
+}
